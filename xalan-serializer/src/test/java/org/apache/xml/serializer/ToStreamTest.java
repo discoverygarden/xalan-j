4d32e0a4e64f84a6f8f00da98d2d1be4a82aff7e
Adam Vessey <adam-vessey@users.noreply.github.com>
2019-08-07 18:15:07 -0300

    Parameterize tests.

--- xalan-serializer/src/test/java/org/apache/xml/serializer/ToStreamTest.java
+++ xalan-serializer/src/test/java/org/apache/xml/serializer/ToStreamTest.java
@@ -7,32 +7,54 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.xml.sax.SAXException;
 
 /**
  * @author adam@discoverygarden.ca
  *
  */
+@RunWith(Parameterized.class)
 public class ToStreamTest {
     private MockStream mock;
     private MockWriter mock_writer;
     
-    private final char HIGH = 55349;
-    private final char LOW = 57109;
-    private final String ESCAPED_DECIMAL_CODEPOINT = "&#120597;";
+    private final static char HIGH = 55349;
+    private final static char LOW = 57109;
+    private final static String ESCAPED_DECIMAL_CODEPOINT = "&#120597;";
+    private final static String CHARACTER = "\uD835\uDF15";
+    
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            {"UTF-8", CHARACTER},
+            {"UTF-16", CHARACTER},
+            {"ASCII", ESCAPED_DECIMAL_CODEPOINT}
+        });
+    }
+    
+    @Parameter
+    public String encoding;
+    @Parameter(1)
+    public String representation;
 
     /**
      * @throws java.lang.Exception
      */
     @Before
     public void setUp() throws Exception {
-        mock = new MockStream();
+        mock = new MockStream(encoding);
         mock_writer = new MockWriter();
         mock.m_writer = mock_writer;
     }
@@ -43,13 +65,15 @@ public class ToStreamTest {
     @After
     public void tearDown() throws Exception {
     }
+    
+    
 
     @Test
     public void testCharacters() {
         try {
             mock.characters(new char [] {'a', 'b', HIGH, LOW, 'c'}, 0, 5);
             mock.flushPending();
-            assertEquals("Output matched", "ab"+ESCAPED_DECIMAL_CODEPOINT+"c", mock_writer.getOutput());
+            assertEquals("Output matched", "ab"+representation+"c", mock_writer.getOutput());
         } catch (SAXException e) {
             fail(e.getMessage());
         }
@@ -61,7 +85,7 @@ public class ToStreamTest {
             mock.characters(new char[] {HIGH}, 0, 1);
             mock.characters(new char[] {LOW}, 0, 1);
             mock.flushPending();
-            assertEquals("Output matched", ESCAPED_DECIMAL_CODEPOINT, mock_writer.getOutput());
+            assertEquals("Output matched", representation, mock_writer.getOutput());
         }
         catch (SAXException e) {
             fail(e.getMessage());
@@ -73,7 +97,7 @@ public class ToStreamTest {
         try {
             mock.characters(new char[] {HIGH, LOW}, 0, 2);
             mock.flushPending();
-            assertEquals("Output matched", ESCAPED_DECIMAL_CODEPOINT, mock_writer.getOutput());
+            assertEquals("Output matched", representation, mock_writer.getOutput());
         }
         catch (SAXException e) {
             fail(e.getMessage());
@@ -105,10 +129,10 @@ public class ToStreamTest {
     }
     
     private class MockStream extends ToStream {
-        public MockStream() {
+        public MockStream(final String encoding) {
             // XXX: Using XML stuff, for convenience.
             m_charInfo = CharInfo.getCharInfo(CharInfo.XML_ENTITIES_RESOURCE, Method.XML);
-            m_encodingInfo = Encodings.getEncodingInfo("ASCII");
+            m_encodingInfo = Encodings.getEncodingInfo(encoding);
             setEscaping(true);
         }
 
